7c4ba6464e71dcf177c9a7855e030db9df0353ce
pngqunshen <pngqunshen@gmail.com>
2023-02-21 15:40:39 +0800

    enabled gazebo for testing

---
+++ src/ee4308_bringup/worlds/world21.sh
@@ -0,0 +1,10 @@
+export TURTLE_X=-2.0
+export TURTLE_Y=-0.5
+export TURTLE_GOALS="1.5,-1 1.4,-3"
+export TURTLE_MIN_X=-8.0
+export TURTLE_MIN_Y=-13.0
+export TURTLE_MAX_X=7.0
+export TURTLE_MAX_Y=3.0
+export HECTOR_X=2.0
+export HECTOR_Y=2.0
+export HECTOR_Z=0.178
